3272b7bb9cc93b4a19bdcc5f165f33cfba907b78
theWickedWebDev <syoung@paintnite.com>
2018-02-16 13:02:48 -0500

    saving

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 curl -X POST --data "payload={\"text\": \":slack: Starting Build - Web\"}" https://hooks.slack.com/services/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
 
-cd /home/ubuntu/coin-collecting-app/ && git pull origin master && npm run build && rm -rf /var/www/html/* && cp -a /home/ubuntu/coin-collecting-app/public/. /var/www/html/
+cd /home/ubuntu/coin-collecting-app/ && git pull origin master && /home/ubuntu/.nvm/versions/node/v8.2.1/bin/npm i && /home/ubuntu/.nvm/versions/node/v8.2.1/bin/npm run build && rm -rf /var/www/html/* && cp -a /home/ubuntu/coin-collecting-app/public/. /var/www/html/
 
 curl -X POST --data "payload={\"text\": \":slack: Build Complete - Web\"}" https://hooks.slack.com/services/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
